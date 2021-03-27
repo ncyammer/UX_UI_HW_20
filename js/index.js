b26e9f25debecdf9555c538a1ef353e03eae55af
ncyammer <72411549+ncyammer@users.noreply.github.com>
2021-03-27 16:10:56 -0700

    Update
    - Iterates on about page
    -Adds Contact page and links up navbar link to it

--- js/index.js
+++ js/index.js
@@ -9,6 +9,32 @@ $('#hamburgermenu').on("click", function(){
     $(".closeMenu").toggle();
 });
 
+$('.tab').on("click", function(){
+    if($(this).hasClass("tab1")){
+        $(".tab1").addClass("active");
+        $(".tab2").removeClass("active");
+        $(".tab3").removeClass("active");
+        $("#tabContent1").css("display", "block");
+        $("#tabContent2").css("display", "none");
+        $("#tabContent3").css("display", "none");
+  }
+      else if($(this).hasClass("tab2")){
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $(".tab3").removeClass("active");
+        $("#tabContent2").css("display", "block");
+        $("#tabContent1").css("display", "none");
+        $("#tabContent3").css("display", "none");
+      }
+   else if($(this).hasClass("tab3")){
+    $(".tab3").addClass("active");
+    $(".tab1").removeClass("active");
+    $(".tab2").removeClass("active");
+    $("#tabContent3").css("display", "block");
+    $("#tabContent1").css("display", "none");
+    $("#tabContent2").css("display", "none");
+  }});
+
 // ALTERNATE CARD HOVER (PAIRED WITH CSS)
 $('.casestudyCard').hover(function(){
     $(this).css("background-size", "110%");
