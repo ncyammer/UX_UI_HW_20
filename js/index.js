9362f76286025961ed2f745d818464547d37f265
ncyammer <72411549+ncyammer@users.noreply.github.com>
2021-03-30 11:30:04 -0700

    Update3
    cleans up old code
    makes nav bar fixed

--- js/index.js
+++ js/index.js
@@ -56,49 +56,8 @@ $('.carousel-item').hover(function(){
     function(){$(this).find('.carousel-caption p').css("opacity", "0");
 });
 
+// var test = ('IT WORKS!')
 // if($(window).width() < 600){
-//     $('#coverLB').modal("display","none"); 
+//     console.log(test);
+//     $('.casestudyCard').find("p").css("display", "none")
 //  } else{}
-
-// var w = screen.width;
-// if (screen.width < 500){
-//     $(".casestudyCard").css("width", w);
-// } else {};
-
-
-// $("body").ready(function(){
-//     $('.mainArea').css('opacity',0)
-//         .slideDown(500)
-//         .animate({opacity:1},500);
-//     $('#mediumCards').css('opacity',0)
-//         .slideDown('slow')
-//         .animate({opacity:1},500);
-// })
-
-
-// $('#mediumCards').ready(function(){
-//     $('.mediumCardstyle').css('display', 'none');
-//     $('.mediumCardstyle').fadeIn(1000);
-// });
-
-// $('#casestudies').ready(function(){
-//     $('.casestudyCard').css('display', 'none');
-//     $('.casestudyCard').fadeIn(500);
-// });
-
-// $(window).on('load', function(){
-//     $('#loadingScreen').fadeIn('5000');
-//     $('#loadingScreen').('5000');
-//     $('#loadingScreen').fadeOut('5000');
-// });
-
-// $(window).on('load', function(){
-//     setTimeout(function(){
-//         $('#loadingScreen').fadeIn('5000');
-//     }, 6000);
-//     $('#loadingScreen').fadeOut('5000');
-// });
-
-// $(document).ready(function(){
-//     $(document).fadeIn(5000);
-// });
